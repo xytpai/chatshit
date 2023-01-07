5cea1b3187e7480d94ec1a11fed2b09f0e276e82
Toy Xu <xytpai@foxmail.com>
2023-01-07 11:32:52 +0800

    Update modeling_bert.py

--- modeling_bert.py
+++ modeling_bert.py
@@ -2,7 +2,47 @@ import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from misc import LinearActivation
+
+
+def gelu(x):
+    return torch.nn.functional.gelu(x, approximate=True)
+
+
+def swish(x):
+    return x * torch.sigmoid(x)
+
+
+ACT2FN = {"gelu": gelu, "tanh": torch.tanh,  "relu": F.relu, "swish": swish}
+class LinearActivation(nn.Module):
+    __constants__ = ['bias']
+    def __init__(self, in_features, out_features, act='gelu', bias=True):
+        super().__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.bias = None
+        assert act in ACT2FN, "Activation function is not found in activation dictionary."
+        self.act_fn = ACT2FN[act]
+        self.weight = nn.Parameter(torch.Tensor(out_features, in_features))
+        if bias:
+            self.bias = nn.Parameter(torch.Tensor(out_features))
+        else:
+            self.register_parameter('bias', None)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        nn.init.kaiming_uniform_(self.weight, a=math.sqrt(5))
+        if self.bias is not None:
+            fan_in, _ = init._calculate_fan_in_and_fan_out(self.weight)
+            bound = 1 / math.sqrt(fan_in)
+            nn.init.uniform_(self.bias, -bound, bound)
+
+    def forward(self, input):
+        return self.act_fn(F.linear(input, self.weight, self.bias))
+
+    def extra_repr(self):
+        return 'in_features={}, out_features={}, bias={}'.format(
+            self.in_features, self.out_features, self.bias is not None
+        )
 
 
 class BertSelfAttention(nn.Module):
@@ -248,9 +288,20 @@ class BertEmbeddings(nn.Module):
 class BertModel(nn.Module):
     def __init__(self, config):
         super().__init__()
+        self.config = config
         self.embeddings = BertEmbeddings(config)
         self.encoder = BertEncoder(config)
         self.pooler = BertPooler(config)
+        self.apply(self.init_bert_weights)
+
+    def init_bert_weights(self, module):
+        if isinstance(module, (nn.Linear, nn.Embedding)):
+            module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
+        elif isinstance(module, nn.LayerNorm):
+            module.bias.data.zero_()
+            module.weight.data.fill_(1.0)
+        if isinstance(module, nn.Linear) and module.bias is not None:
+            module.bias.data.zero_()
 
     def forward(self, input_ids, token_type_ids, attention_mask):
         if attention_mask is None:
